f912049b6f350782e350d257edacf241c185d8de
mahmoud youssef <myoussef@advantekwms.com>
2025-01-16 19:47:31 +0200

    Complete the communication between Basket.API and Discount.Grpc microservices

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,34 +1,55 @@
 
+using Discount.Grpc;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using System.Net.Security;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+
+//Application Services
 var assembly = typeof(Program).Assembly;
-// Add Services to the Container
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssemblies(assembly);
+    config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 
+//Data Services
 builder.Services.AddMarten(opts =>
 {
-    opts.Connection(builder.Configuration.GetConnectionString("Database") ?? throw new Exception("Invalid database connection"));
-    opts.Schema.For<ShoppingCart>().Identity(e => e.UserName);
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepositry>();
 builder.Services.Decorate<IBasketRepository, CachedBasketRepositry>();
-builder.Services.AddStackExchangeRedisCache(opts =>
+
+builder.Services.AddStackExchangeRedisCache(options =>
 {
-    opts.Configuration = builder.Configuration.GetConnectionString("Redis");
-    //opts.InstanceName = "Basket";
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+    //options.InstanceName = "Basket";
 });
 
+//Grpc Services
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    return new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+});
+
+//Cross-Cutting Services
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 builder.Services.AddHealthChecks()
     .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
     .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
