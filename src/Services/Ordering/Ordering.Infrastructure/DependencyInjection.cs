04b2358e379ce4a29df567c633023e1957bc7f02
Mahmoud\Mahmoud-PC <myoussef@advantekwms.com>
2025-02-11 15:19:53 +0200

    fix  Ordering.Infrastructure layer

---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿namespace Ordering.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database")!;
+
+        // Add Services to Container
+        //services.AddDbContext<ApplicationDbContext>(opts => opts.useSqlServer(connectionString));
+        //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+        return services;
+    }
+}
